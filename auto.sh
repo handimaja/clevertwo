7dbc529f9b52b4f8aad7d6df7ed263eef01eaaad
handimaja <91892204+handimaja@users.noreply.github.com>
2021-12-05 06:59:24 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-cd /home/ganti/clevertwo
+cd /home/a/clevertwo
 d=`date '+%Y-%m-%dT%H:%M:%SZ'`
 echo $d > LAST_UPDATED
 git config --local user.email "handimaja@outlook.com"
 git config --local user.name "handimaja"
 git commit -a -m "new update"
-git push clever master
+git push a master
